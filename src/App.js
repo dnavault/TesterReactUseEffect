f6f2f7bc84221bf7465c144cd369385ec49e7f95
dnavault <47720676+dnavault@users.noreply.github.com>
2021-02-23 09:39:21 -0500

    ReactAG refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./styles.css";
 import WorkOrderList from "./components/WorkOrderList";
-import ReactAG from "./components/ReactAG";
 
 function App() {
   return (
     <div className="App">
+      {console.log(WorkOrderList)}
       <div>
-        <div><ReactAG/></div>
         <h1>Work Orders</h1>
         <WorkOrderList />
       </div>
